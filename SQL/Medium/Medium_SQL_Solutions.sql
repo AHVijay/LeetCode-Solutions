8d4230730f0309537d278cdc425da38393f940d9
VijayAH <vijayahiremath9@gmail.com>
2026-02-01 07:28:43 +0530

    Added more SQL Solutions

--- SQL/Medium/Medium_SQL_Solutions.sql
+++ SQL/Medium/Medium_SQL_Solutions.sql
@@ -52,6 +52,23 @@ Confirmations
 +----------------+----------+
 
 Approach:
-Use 
+Use LEFT JOIN to join the two tables and use CASE statement to find the confirmation rate of each user.
 
 */
+
+SELECT 
+    s.user_id,
+    ROUND(
+        IFNULL(
+            SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END) / COUNT(c.action),
+            0
+        ),
+        2
+    ) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
+    ON s.user_id = c.user_id
+GROUP BY s.user_id;
+
+
+
