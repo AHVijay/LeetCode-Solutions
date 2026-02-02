120d9bc55fa9e0958a09cb805616420c44cc376f
VijayAH <vijayahiremath9@gmail.com>
2026-02-02 23:46:55 +0530

    Added more Solutions

--- SQL/Easy/6_SQL_Solutions.sql
+++ SQL/Easy/6_SQL_Solutions.sql
@@ -267,3 +267,52 @@ LEFT JOIN UnitsSold u
     ON p.product_id = u.product_id
     AND u.purchase_date BETWEEN p.start_date AND p.end_date
 GROUP BY p.product_id;
+
+
+/*
+15) Problem: Project Employees I
+LeetCode: https://leetcode.com/problems/project-employees-i/description/?envType=study-plan-v2&envId=top-sql-50
+
+Tables:
+Project
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| project_id  | int     |
+| employee_id | int     |
++-------------+---------+
+(project_id, employee_id) is the primary key of this table.
+employee_id is a foreign key to Employee table.
+Each row of this table indicates that the employee with employee_id is working on the project with project_id.
+ 
+
+Table: Employee
+
++------------------+---------+
+| Column Name      | Type    |
++------------------+---------+
+| employee_id      | int     |
+| name             | varchar |
+| experience_years | int     |
++------------------+---------+
+employee_id is the primary key of this table. It's guaranteed that experience_years is not NULL.
+Each row of this table contains information about one employee.
+
+Description:
+
+Write an SQL query that reports the average experience years of all the employees for each project, rounded to 2 digits.
+Return the result table in any order.
+
+Approach:
+Use LEFT JOIN to join the two tables and use CASE statement to find the average experience years of all the employees for each project.
+
+*/
+
+SELECT
+    p.project_id, ROUND(AVG(e.experience_years), 2) as average_years
+FROM
+    Project p
+LEFT JOIN Employee e 
+    ON p.employee_id = e.employee_id
+GROUP BY p.project_id;
