bbf1f06523c5106d20e8b09969412609567e14cc
VijayAH <vijayahiremath9@gmail.com>
2026-02-03 19:47:50 +0530

    Added more Solutions

--- SQL/Easy/6_SQL_Solutions.sql
+++ SQL/Easy/6_SQL_Solutions.sql
@@ -377,3 +377,49 @@ FROM Register r
 CROSS JOIN total_users t
 GROUP BY r.contest_id, t.cnt
 ORDER BY percentage DESC, r.contest_id ASC;
+
+
+/*
+17) Problem: Queries Quality and Percentage
+LeetCode: https://leetcode.com/problems/queries-quality-and-percentage/?envType=study-plan-v2&envId=top-sql-50
+
+Tables:
+Queries
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| query_name  | varchar |
+| result      | varchar |
+| position    | int     |
+| rating      | int     |
++-------------+---------+
+This table may have duplicate rows.
+This table contains information collected from some queries on a database.
+The position column has a value from 1 to 500.
+The rating column has a value from 1 to 5. Query with rating less than 3 is a poor query.
+
+Description:
+
+We define query quality as -> The average of the ratio between query rating and its position.
+We also define poor query percentage as -> The percentage of all queries with rating less than 3.
+
+Write a solution to find each query_name, the quality and poor_query_percentage.
+Both quality and poor_query_percentage should be rounded to 2 decimal places.
+
+Return the result table in any order.
+
+Approach:
+1) Use CASE statement to find the quality and poor_query_percentage.
+2) Use WHERE clause to filter out the rows where query_name is NULL.
+3) Use GROUP BY to group the result table by query_name.
+
+*/
+
+SELECT 
+    query_name, 
+    ROUND(AVG(rating / position), 2) AS quality,
+    ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100 / COUNT(*), 2) AS poor_query_percentage
+FROM Queries
+WHERE query_name IS NOT NULL
+GROUP BY query_name;
