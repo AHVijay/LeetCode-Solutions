102a568bdd9874535a2c4a8f548bfca9e217e722
VijayAH <vijayahiremath9@gmail.com>
2026-02-05 22:01:29 +0530

    Updated more solutions

--- SQL/Easy/6_SQL_Solutions.sql
+++ SQL/Easy/6_SQL_Solutions.sql
@@ -522,4 +522,36 @@ WHERE
 GROUP BY activity_date;
 
 
+/*
+_______________________________________________________________________________________________________________
+20) Problem: Classes With at Least 5 Students
+LeetCode: https://leetcode.com/problems/user-activity-for-the-past-30-days-i/description/?envType=study-plan-v2&envId=top-sql-50
 
+Tables:
+Courses
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| student     | varchar |
+| class       | varchar |
++-------------+---------+
+(student, class) is the primary key (combination of columns with unique values) for this table.
+Each row of this table indicates the name of a student and the class in which they are enrolled.
+
+Description:
+
+Write a solution to find all the classes that have at least five students.
+Return the result table in any order.
+
+Approach 1:
+1) Use GROUP BY to group the result table by class.
+2) Use HAVING to filter the classes that have at least five students.
+*/
+
+SELECT
+    class
+FROM
+    Courses
+GROUP BY class
+HAVING COUNT(*) >= 5;
