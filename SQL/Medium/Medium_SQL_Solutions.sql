11335ad74472f59e5fe331bc4a19c7382f3fd26c
VijayAH <vijayahiremath9@gmail.com>
2026-02-08 22:36:39 +0530

    Updating more solutions

--- SQL/Medium/Medium_SQL_Solutions.sql
+++ SQL/Medium/Medium_SQL_Solutions.sql
@@ -576,3 +576,40 @@ LEFT JOIN (
     WHERE rn = 1
 ) latest_prices
 ON all_products.product_id = latest_prices.product_id;
+
+
+/*
+_____________________________________________________________________________________________________________
+10) Problem: Last Person to Fit in the Bus
+LeetCode: https://leetcode.com/problems/last-person-to-fit-in-the-bus/description/?envType=study-plan-v2&envId=top-sql-50
+
+Tables:
+
+Queue
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| person_id   | int     |
+| person_name | varchar |
+| weight      | int     |
+| turn        | int     |
++-------------+---------+
+person_id column contains unique values.
+This table has the information about all people waiting for a bus.
+The person_id and turn columns will contain all numbers from 1 to n, where n is the number of rows in the table.
+turn determines the order of which the people will board the bus, where turn=1 denotes the first person to board and turn=n denotes the last person to board.
+weight is the weight of the person in kilograms.
+
+Description:
+
+There is a queue of people waiting to board a bus. However, the bus has a weight limit of 1000 kilograms, 
+so there may be some people who cannot board.
+Write a solution to find the person_name of the last person that can fit on the bus without exceeding the weight limit. 
+The test cases are generated such that the first person does not exceed the weight limit.
+
+Note that only one person can board the bus at any given turn.
+
+Approach 1:
+1) Use UNION to combine the two queries.
+*/
+
