08cb676eb505a4b1892b485dc1f3fa8411d11213
VijayAH <vijayahiremath9@gmail.com>
2026-02-09 23:32:53 +0530

    Updated more solutions

--- SQL/Easy/6_SQL_Solutions.sql
+++ SQL/Easy/6_SQL_Solutions.sql
@@ -847,7 +847,31 @@ but the reports still have their manager_id set to the manager that left.
 Return the result table ordered by employee_id.
 
 Approach 1: MYSQL APPROACH
-1) Use 
+1) Use NOT IN to find the employees whose manager left the company.
 */
 
+SELECT employee_id
+FROM Employees
+    WHERE salary < 30000 AND
+    manager_id IS NOT NULL AND
+    manager_id NOT IN
+    (
+        SELECT employee_id
+        FROM EMployees
+    )
+ORDER BY employee_id;
 
+/*
+_______________________________________________________________________________________________________________
+Approach 2: 
+1) Use LEFT JOIN to find the employees whose manager left the company.
+*/
+
+SELECT e1.employee_id
+FROM Employees e1
+LEFT JOIN Employees e2
+    ON e1.manager_id = e2.employee_id
+WHERE e1.salary < 30000
+  AND e1.manager_id IS NOT NULL
+  AND e2.employee_id IS NULL
+ORDER BY e1.employee_id;
