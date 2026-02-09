01eb6b431dd17a6a92f11e04f7e45e9e639645ee
VijayAH <vijayahiremath9@gmail.com>
2026-02-09 23:52:29 +0530

    Updated more solutions

--- SQL/Medium/Medium_SQL_Solutions.sql
+++ SQL/Medium/Medium_SQL_Solutions.sql
@@ -741,3 +741,43 @@ LEFT JOIN (
     GROUP BY category
 ) t
 ON c.category = t.category;
+
+
+/*
+_____________________________________________________________________________________________________________
+12) Problem: Exchange Seats
+LeetCode: https://leetcode.com/problems/exchange-seats/description/?envType=study-plan-v2&envId=top-sql-50
+
+Tables:
+
+Seat
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| student     | varchar |
++-------------+---------+
+id is the primary key (unique value) column for this table.
+Each row of this table indicates the name and the ID of a student.
+The ID sequence always starts from 1 and increments continuously.
+
+Description:
+
+Write a solution to swap the seat id of every two consecutive students. If the number of students is odd, 
+the id of the last student is not swapped.
+
+Return the result table ordered by id in ascending order.
+
+Approach 1:
+1) Use CASE statement to swap the seat id of every two consecutive students.
+*/
+
+SELECT 
+    CASE
+        WHEN id % 2 = 1 AND id < (SELECT COUNT(*) FROM Seat) THEN id + 1
+        WHEN id % 2 = 1 AND id = (SELECT COUNT(*) FROM Seat) THEN id
+        ELSE id - 1
+    END AS id,
+    student
+FROM Seat
+ORDER BY id;
