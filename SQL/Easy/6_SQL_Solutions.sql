bfdc1596b6c7e75d5755ef09f258388fdb5a1122
VijayAH <vijayahiremath9@gmail.com>
2026-02-14 23:31:28 +0530

    Added more solutions

--- SQL/Easy/6_SQL_Solutions.sql
+++ SQL/Easy/6_SQL_Solutions.sql
@@ -947,3 +947,46 @@ SELECT patient_id,
 FROM Patients
 WHERE conditions LIKE 'DIAB1%'       
    OR conditions LIKE '% DIAB1%';
+
+/*
+_______________________________________________________________________________________________________________
+29) Problem: Delete Duplicate Emails
+LeetCode: https://leetcode.com/problems/delete-duplicate-emails/description/?envType=study-plan-v2&envId=top-sql-50
+
+Tables:
+
+Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+
+Description:
+
+Write a solution to delete all duplicate emails, keeping only one unique email with the smallest id.
+
+For SQL users, please note that you are supposed to write a DELETE statement and not a SELECT one.
+
+For Pandas users, please note that you are supposed to modify Person in place.
+
+After running your script, the answer shown is the Person table. 
+The driver will first compile and run your piece of code and then show the Person table. 
+The final order of the Person table does not matter.
+
+Approach 1: MYSQL APPROACH
+1) Use DELETE statement to delete all duplicate emails, keeping only one unique email with the smallest id.
+*/
+
+DELETE FROM Person
+WHERE id NOT IN (
+SELECT min_id FROM (
+SELECT MIN(id) AS min_id
+FROM Person
+GROUP BY email
+) AS t
+);
