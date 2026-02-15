7e6199045ba669314c10f51be66693d14d2ac283
VijayAH <vijayahiremath9@gmail.com>
2026-02-15 21:57:53 +0530

    Added more solutions

--- SQL/Medium/Medium_SQL_Solutions.sql
+++ SQL/Medium/Medium_SQL_Solutions.sql
@@ -1040,3 +1040,31 @@ AND CONCAT(lat, ',', lon) IN (
     GROUP BY lat, lon
     HAVING COUNT(*) = 1
 );
+
+/*
+_____________________________________________________________________________________________________________
+16) Problem: Second Highest Salary
+LeetCode: https://leetcode.com/problems/second-highest-salary/description/?envType=study-plan-v2&envId=top-sql-50
+
+Tables:
+
+Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains information about the salary of an employee.
+
+Description:
+
+Write a solution to find the second highest distinct salary from the Employee table. 
+If there is no second highest salary, return null (return None in Pandas).
+
+Approach 1:
+1) Use 
+*/
+
