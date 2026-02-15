e2fde00b08828a8e8dff73407ea209f302390759
VijayAH <vijayahiremath9@gmail.com>
2026-02-15 23:38:05 +0530

    Committed Group Sold Products By The Date

--- SQL/Easy/6_SQL_Solutions.sql
+++ SQL/Easy/6_SQL_Solutions.sql
@@ -990,3 +990,58 @@ FROM Person
 GROUP BY email
 ) AS t
 );
+
+/*
+_______________________________________________________________________________________________________________
+30) Problem: Group Sold Products By The Date
+LeetCode: https://leetcode.com/problems/group-sold-products-by-the-date/description/?envType=study-plan-v2&envId=top-sql-50
+
+Tables:
+
+Activities:
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| sell_date   | date    |
+| product     | varchar |
++-------------+---------+
+There is no primary key (column with unique values) for this table. It may contain duplicates.
+Each row of this table contains the product name and the date it was sold in a market.
+
+Description:
+
+Write a solution to find for each date the number of different products sold and their names.
+
+The sold products names for each date should be sorted lexicographically.
+
+Return the result table ordered by sell_date.
+
+Approach 1: MYSQL APPROACH
+1) Use GROUP_CONCAT to concatenate distinct products
+*/
+
+SELECT
+    sell_date,
+    COUNT(DISTINCT product) AS num_sold,
+    GROUP_CONCAT(DISTINCT product ORDER BY product SEPARATOR ',') AS products
+FROM Activities
+GROUP BY sell_date
+ORDER BY sell_date;
+
+/*
+Approach 2: SQL SERVER APPROACH
+1) Use STRING_AGG to concatenate distinct products
+Note: For SQL Server, we need to use a subquery to get distinct products first
+*/
+
+SELECT
+    sell_date,
+    COUNT(DISTINCT product) AS num_sold,
+    STRING_AGG(product, ',') WITHIN GROUP (ORDER BY product) AS products
+FROM (
+    SELECT DISTINCT sell_date, product
+    FROM Activities
+) AS distinct_products
+GROUP BY sell_date
+ORDER BY sell_date;
