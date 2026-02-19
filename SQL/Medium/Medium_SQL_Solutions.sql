5df96de11b0a28cbc2ca96a64f6b1a0a13820daa
VijayAH <vijayahiremath9@gmail.com>
2026-02-19 23:15:57 +0530

    Added more solutions

--- SQL/Medium/Medium_SQL_Solutions.sql
+++ SQL/Medium/Medium_SQL_Solutions.sql
@@ -1114,3 +1114,39 @@ SELECT (
     ) ranked
     WHERE rnk = 2
 ) AS SecondHighestSalary;
+
+
+/*
+_____________________________________________________________________________________________________________
+16) Problem: Tree Node
+LeetCode: https://leetcode.com/problems/tree-node/description/?envType=problem-list-v2&envId=db-db3-grouping-aggregation
+
+Tables:
+
+Tree
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| p_id        | int  |
++-------------+------+
+id is the column with unique values for this table.
+Each row of this table contains information about the id of a node and the id of its parent node in a tree.
+The given structure is always a valid tree.
+
+Description:
+
+Each node in the tree can be one of three types:
+
+"Leaf": if the node is a leaf node.
+"Root": if the node is the root of the tree.
+"Inner": If the node is neither a leaf node nor a root node.
+Write a solution to report the type of each node in the tree.
+
+Return the result table in any order.
+
+Approach 1:
+1) Use 
+*/
+
