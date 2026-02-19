ae080babe42d3e26b7916f2dee18128b4a46d11c
VijayAH <vijayahiremath9@gmail.com>
2026-02-20 01:28:29 +0530

    Committed Solution Rank Scores

--- SQL/Medium/Medium_SQL_Solutions.sql
+++ SQL/Medium/Medium_SQL_Solutions.sql
@@ -1178,3 +1178,40 @@ SELECT
         else 'Inner'
     end as type
 from Tree t1
+
+
+/*
+_____________________________________________________________________________________________________________
+17) Problem: Rank Scores
+LeetCode: https://leetcode.com/problems/rank-scores/?envType=problem-list-v2&envId=db-db4-window-functions-ranking
+
+Tables:
+
+Scores
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| score       | decimal |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains the score of a game. Score is a floating point value with two decimal places.
+
+Description:
+
+Write a solution to find the rank of the scores. The ranking should be calculated according to the following rules:
+
+The scores should be ranked from the highest to the lowest.
+If there is a tie between two scores, both should have the same ranking.
+After a tie, the next ranking number should be the next consecutive integer value. In other words, there should be no holes between ranks.
+Return the result table ordered by score in descending order.
+
+Approach 1:
+1) Use DENSE_RANK() to assign a rank to each score.
+2) Use ORDER BY to sort the result table by score in descending order.
+*/
+
+SELECT
+score,
+DENSE_RANK() OVER (ORDER BY score DESC) AS rank
+FROM Scores;
