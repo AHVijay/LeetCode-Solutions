96041d8e309aff345f529b1db4a315735f026ee8
VijayAH <vijayahiremath9@gmail.com>
2026-02-22 00:02:40 +0530

    Committed FzzBuzz Solution

--- Python/Easy/Basic_Pandas.py
+++ Python/Easy/Basic_Pandas.py
@@ -394,3 +394,35 @@ def checkOddEven(self, x):
         return "Even"
     else:
         return "Odd"
+    
+
+"""
+18. Problem: The FizzBuzz
+GeeksForGeeks: https://www.geeksforgeeks.org/problems/the-fizzbuzz-program/1?page=1&category=python&sortBy=submissions
+
+Description:
+You are given a number  and you have to print your answer according to the following:
+If the number is divisible by 3, you print "Fizz" (without quotes)
+If the number is divisible by 5, you print "Buzz" (without quotes)
+If the number is divisible by both 3 and 5, you print "FizzBuzz" (without quotes)
+In any other case, you print the number itself
+
+Note: You should add a newline character after print() statement.
+
+Approach:
+1) Use if-elif-else to check the status.
+
+Time Complexity: O(1)
+Space Complexity: O(1)
+"""
+
+def fizzBuzz(number):
+    # Write your code here.
+    if number % 15 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
